991f5432c99124cc8e3fbe11b316aed1b0338ac8
Aenri <162776466+ImAenri@users.noreply.github.com>
2024-09-23 22:46:24 +0800

    added transition (unfinished)
    added transition between webpages for seamless redirection

---
+++ wheelz/src/transition.js
@@ -0,0 +1,17 @@
+import { animate, motion } from "framer-motion";
+
+const animations = {
+    initial: {opacity: 0, x: 100},
+    animate: {opacity: 1, x: 0},
+    exit: {opacity: 0, x: -100}
+}
+
+const transition = ({children}) => {
+    return () => (
+        <>
+            <motion.div variants={animations} initial="initial" animate="animate" exit="exit">
+            {children}
+            </motion.div>
+        </>
+    )
+}
