633de23fcc6a71626a5e2bd2ba79c3e8988cec45
Aenri <162776466+ImAenri@users.noreply.github.com>
2024-11-09 00:05:58 +0800

    added more sections(wip)
    updated cars page and about us page

--- wheelz/src/about.js
+++ wheelz/src/about.js
@@ -1,5 +1,6 @@
 import './about.css';
 import Navbar from './navbar';
+import {Container, Row, Col} from 'react-bootstrap';
 
 const about = () => {
     return (
@@ -8,7 +9,49 @@ const about = () => {
             <div className='mainSection'>
                 <div className='coreText'>
                     <h1>About <span style={{color:"#82060D"}}>Us</span></h1>
-                    <p>Get to know the team behind Wheelz!</p>
+                    <p>Meet the team behind Wheelz!</p>
+                </div>
+                <div>
+                <Container fluid style={{backgroundColor:"#EDEDED"}}>
+                    <center>
+                    <Row style={{marginBottom:'50px'}} className='subtitle'>
+                        <Col md={2}>
+                            <img src='../Pop.gif' width={250} height={250}/>
+                        </Col>
+                        <Col md={2} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                            <p>Nathaniel Caballero</p>
+                        </Col>
+                        <Col md={2}>
+                            <img src='../Pop.gif' width={250} height={250}/>
+                        </Col>
+                        <Col md={2} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                            <p>Erika Castro</p>
+                        </Col>
+                        <Col md={2}>
+                            <img src='../Pop.gif' width={250} height={250}/>
+                        </Col>
+                        <Col md={2} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                            <p>Jovie Cleofas</p>
+                        </Col>
+                    </Row>
+                    </center>
+                    <center>
+                    <Row className='subtitle'>
+                        <Col md={4}>
+                            <img src='../Pop.gif' width={250} height={250}/>
+                        </Col>
+                        <Col md={2} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                            <p>Joaquin Maravilla</p>
+                        </Col>
+                        <Col md={4}>
+                            <img src='../Pop.gif' width={250} height={250}/>
+                        </Col>
+                        <Col md={2} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                            <p>Diane Sula</p>
+                        </Col>
+                    </Row>
+                    </center>
+                </Container>
                 </div>
 
             </div>
--- wheelz/src/cars.js
+++ wheelz/src/cars.js
@@ -1,5 +1,9 @@
 import './cars.css';
 import Navbar from './navbar';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const cars = () => {
     return (
@@ -8,9 +12,27 @@ const cars = () => {
             <div className='mainSection'>
                 <div className='coreText'>
                     <h1>Available <span style={{color:"#82060D"}}>Cars</span></h1>
-                    <p>Choose the car of your preference and ???</p>
+                    <p>Choose the car of your preference and rent them!</p>
                 </div>
-
+            </div>
+            <div>
+            <Row xs={1} md={3} className="g-4">
+            {Array.from({ length: 3 }).map((_, idx) => (
+                <Col key={idx}>
+                    <Card className='bg-light title' style={{ width: '25rem', height: '33rem', fontSize: '30px', borderRadius:'25px'}}>
+                    <Card.Img className='imgPosition' src="../city.png" />
+                    <Card.ImgOverlay style={{padding:0}}>
+                    <Card.Body>
+                            <p style={{marginLeft:'10px'}}>Honda</p>
+                            <h1 style={{position:'absolute', fontSize:'150px', color:'gray', marginTop:'-40px'}}>CITY</h1>
+                            <p style={{position:'absolute', marginLeft:'10px', marginTop:'370px'}}>P999/day</p>
+                            <Button className='rentNowBtn'>Rent Now</Button>
+                    </Card.Body>
+                    </Card.ImgOverlay>
+                    </Card>
+            </Col>
+            ))}
+            </Row>
             </div>
         </div>
     )
