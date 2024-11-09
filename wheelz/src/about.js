98de5b8eebb2486cbcd4c57d2ee32c814f8f9d96
CIITSHSDTP00\CIIT <jovie.cleofas@ciit.edu.ph>
2024-11-09 17:45:42 +0800

    updated css(still wip)
    updated css for about, and membership pages

--- wheelz/src/about.js
+++ wheelz/src/about.js
@@ -9,26 +9,46 @@ const about = () => {
             <div className='mainSection'>
                 <div className='coreText'>
                     <h1>About <span style={{color:"#82060D"}}>Us</span></h1>
-                    <p>Meet the team behind Wheelz!</p>
+                    <p>Get to learn the company behind your service!</p>
+                </div>
+                <div>
+                <Container fluid style={{backgroundColor:"#EDEDED"}}>
+                <center>
+                    <Row style={{marginBottom:'50px'}} className='subtitle'>
+                        <Col md={6}>
+                            <h1 className='title'>Mission</h1>
+                            <p className='subtitle'>To provide reliable, and convenient car rentals with a focus on customer satisfaction and safety.</p>
+                        </Col>
+                        <Col md={6}>
+                            <h1 className='title' style={{color:"#82060D"}}>Vision</h1>
+                            <p className='subtitle'>To be the leading car rental service, known for its quality, innovation, security, and sustainable practices.</p>
+                        </Col>
+                    </Row>
+                </center>
+                </Container>
+                </div>
+                <div className='coreText' style={{marginBottom:'30px'}}>
+                    <h1>The <span style={{color:"#82060D"}}>Team</span></h1>
+                    <p>Meet the team of Wheelz!</p>
                 </div>
                 <div>
                 <Container fluid style={{backgroundColor:"#EDEDED"}}>
                     <center>
                     <Row style={{marginBottom:'50px'}} className='subtitle'>
                         <Col md={2}>
-                            <img src='../Pop.gif' width={250} height={250}/>
+                            <img src='../Pop.gif' alt='pic of Nathan Caballero' width={250} height={250}/>
                         </Col>
                         <Col md={2} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
                             <p>Nathaniel Caballero</p>
                         </Col>
                         <Col md={2}>
-                            <img src='../Pop.gif' width={250} height={250}/>
+                            <img src='../Pop.gif' alt='pic of Erika Castro' width={250} height={250}/>
                         </Col>
                         <Col md={2} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
                             <p>Erika Castro</p>
                         </Col>
                         <Col md={2}>
-                            <img src='../Pop.gif' width={250} height={250}/>
+                            <img src='../Pop.gif' alt='pic of Jovie Cleofas' width={250} height={250}/>
                         </Col>
                         <Col md={2} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
                             <p>Jovie Cleofas</p>
@@ -38,13 +58,13 @@ const about = () => {
                     <center>
                     <Row className='subtitle'>
                         <Col md={4}>
-                            <img src='../Pop.gif' width={250} height={250}/>
+                            <img src='../Pop.gif' alt='pic of Joaquin Maravilla' width={250} height={250}/>
                         </Col>
                         <Col md={2} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
                             <p>Joaquin Maravilla</p>
                         </Col>
                         <Col md={4}>
-                            <img src='../Pop.gif' width={250} height={250}/>
+                            <img src='../Pop.gif' alt='pic of Diane Sula' width={250} height={250}/>
                         </Col>
                         <Col md={2} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
                             <p>Diane Sula</p>
--- wheelz/src/membership.js
+++ wheelz/src/membership.js
@@ -1,5 +1,9 @@
 import './membership.css';
 import Navbar from './navbar';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 const membership = () => {
     return (
@@ -10,14 +14,32 @@ const membership = () => {
                     <h1>Membership <span style={{color:"#82060D"}}>Plans</span></h1>
                     <p>Choose the right plan for you and enjoy exclusive rewards!</p>
                 </div>
-                <div className='membershipCards'>
-                    <div className='standardCard'>
+                <Row className="g-4">
+                <center>
+                <div style={{display:"flex"}}>
+                <Col md={6}>
+                <a href='#' className='stndrdCard'>
+                <Card className='hoverStndrd' style={{width:'35rem', height:'45rem', padding:'20px'}}>
+                    
+                    <h1 className='title'>Standard</h1>
+                    <Card.Body style={{marginTop:"5rem"}}>• This is some text within a card body.</Card.Body>
+                </Card>
+                </a>
+                </Col>
+                <Col md={6}>
+                <a href='#' className='premCard' style={{textDecoration:'none'}}>
+                <Card className='hoverPrem' style={{width:'35rem', height:'45rem', padding:'20px'}}>
+                    <h1 className='title'>Premium</h1>
+                    <Card.Body style={{marginTop:"5rem"}}>• This is some text within a card body.
+                        <br></br><br></br>                • This is some text within a card body.
 
-                    </div>
-                    <div className='premiumCard'>
-
-                    </div>
+                    </Card.Body>
+                </Card>
+                </a>
+                </Col>
                 </div>
+                </center>
+                </Row>
             </div>
         </div>
     )
